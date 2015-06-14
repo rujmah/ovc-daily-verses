17f3c988e18bfa61336779edb687e15c16b91227
Robin Mayfield <rjjm@rjjm.net>
2015-06-14 22:58:10 +0100

    feat(todays-verses): displays todays verses

--- frontend/js/controllers/MainController.js
+++ frontend/js/controllers/MainController.js
@@ -1,12 +1,15 @@
 'use static'
 
-app.controller('MainController', ['$scope', function($scope) {
+app.controller('MainController', ['$scope', '$http', function($scope, $http) {
   $scope.title = 'Here are some verses';
 
   $scope.day_of_month = moment().format('Do');
   $scope.month_name = moment().format('MMMM');
 
-
+  $http.get('/verses/june_2015.json').success(function (data, status, headers, config) {
+    $scope.verses = data;
+    $scope.todays_verses = _.find(data, {day: moment().date()});
+  });
 
 }]);
 
